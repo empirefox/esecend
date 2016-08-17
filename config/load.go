1f733d4a2e34c3486a83c45ee23d34b575037e49
empirefox <empirefoxit@sina.com>
2016-08-17 13:41:18 +0800

    add config.Load

---
+++ config/load.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"io/ioutil"
+
+	"github.com/rolldever/go-json5"
+)
+
+func Load(jsonPath string) (*Config, error) {
+	bDoc, err := ioutil.ReadFile(jsonPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	if err = json5.Unmarshal(bDoc, &config); err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
