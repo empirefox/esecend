156106649d4df14a704757b43d86cb752b958bc5
empirefoxit <empirefoxit@sina.com>
2016-09-10 20:58:40 +0800

    add handler to OrderHub

--- hub/order-hub.go
+++ hub/order-hub.go
@@ -1,10 +1,20 @@
 package hub
 
-import "github.com/empirefox/esecend/db-service"
+import (
+	"github.com/empirefox/esecend/db-service"
+	"github.com/golang/glog"
+)
 
 type OrderHub struct {
 	dbs       *dbsrv.DbService
 	chanInput chan interface{}
+}
+
+func NewOrderHub(dbs *dbsrv.DbService) *OrderHub {
+	return &OrderHub{
+		dbs:       dbs,
+		chanInput: make(chan interface{}, 100),
+	}
 }
 
 func (hub *OrderHub) Run() {
@@ -14,6 +24,18 @@ func (hub *OrderHub) Run() {
 			hub.onPrepayOrder(in)
 		case *payOrderInput:
 			hub.onPayOrder(in)
+		case *orderPaidStateInput:
+			hub.onOrderPaidState(in)
+		case *orderOnWxPayNotifyInput:
+			hub.onWxPayNotify(in)
+		case *orderChangeStateInput:
+			hub.onOrderChangeState(in)
+		case *orderMgrStateInput:
+			hub.onMgrOrderState(in)
+		case *orderEvalInput:
+			hub.onEvalSave(in)
+		default:
+			glog.Errorf("cannot handle input: %#v\n", in)
 		}
 	}
 }
