3a9f5bbe123876c947bb2d13f8dc06781e8073a4
empirefox <empirefoxit@sina.com>
2016-09-21 22:28:10 +0800

    add Billboard to carousel

--- front/carouse.go
+++ front/carouse.go
@@ -1,12 +1,21 @@
 //go:generate reform
 package front
 
+type BillboardType int
+
+const (
+	TBillboardUnknow BillboardType = iota
+	TBillboardHome
+	TBillboardNews
+)
+
 //reform:cc_carousel
 type CarouselItem struct {
-	ID         uint   `reform:"id,pk"`
-	Img        string `reform:"img"`
-	Link       string `reform:"link"`
-	ProductID  uint   `reform:"product_id"`
-	CategoryID uint   `reform:"category_id"`
-	Pos        int64  `reform:"pos"`
+	ID         uint          `reform:"id,pk"`
+	Img        string        `reform:"img"`
+	Link       string        `reform:"link"`
+	ProductID  uint          `reform:"product_id"`
+	CategoryID uint          `reform:"category_id"`
+	Pos        int64         `reform:"pos"`
+	Billboard  BillboardType `reform:"billboard"`
 }
