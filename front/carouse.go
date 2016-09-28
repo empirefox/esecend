6f3e628ac7ceeedf77255e33f5a9697929d8e8c2
empirefox <empirefoxit@sina.com>
2016-09-28 21:17:39 +0800

    add special to carousel

--- front/carouse.go
+++ front/carouse.go
@@ -16,6 +16,7 @@ type CarouselItem struct {
 	Img        string        `reform:"img"`
 	Link       string        `reform:"link"`
 	ProductID  uint          `reform:"product_id"`
+	SpecialID  uint          `reform:"special_id"`
 	CategoryID uint          `reform:"category_id"`
 	Billboard  BillboardType `reform:"billboard"`
 	Pos        int64         `reform:"pos"`
